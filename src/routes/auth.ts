9096ce0d910fe6efb275fa0e65536a465c3beb71
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-11-03 11:05:29 -0300

    feat: create jwt validation

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,13 +1,20 @@
 import { FastifyInstance } from "fastify"
 import { z } from "zod";
 import { prisma } from "../lib/prisma"
+import { authenticate } from "../plugins/authenticate";
 
-export async function authRoutes(fastify:  FastifyInstance) {
-  const createUserBody = z.object({
-    access_token: z.string(),
+export async function authRoutes(fastify: FastifyInstance) {
+  fastify.get('/me', {
+    onRequest: [authenticate]
+  }, async (request) => {
+    return { user: request.user }
   })
 
   fastify.post('/users', async (request) => {
+    const createUserBody = z.object({
+      access_token: z.string(),
+    })
+
     const { access_token } = createUserBody.parse(request.body);
 
     const userResponse = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
@@ -32,7 +39,7 @@ export async function authRoutes(fastify:  FastifyInstance) {
       where: {
         googleId: userInfo.id,
       }
-    }) 
+    })
 
     if (!user) {
       user = await prisma.user.create({
